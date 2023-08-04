b30e220da50fe184c95218946c1876eb7c913cff
Harshal Sanghvi <harshal2030@outlook.com>
2023-08-04 14:52:10 +0530

    completed gophercise #2

---
+++ urlshort/README.md
@@ -0,0 +1,3 @@
+# Web Server
+
+It's a simple web server that redirects short urls to destination based on the map defined in yml. To run server `go run main.go`
---
+++ urlshort/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"urlshort/urlshort"
+)
+
+func main() {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+
+	yaml, err := os.ReadFile("paths.yml")
+
+	if err != nil {
+		panic(err)
+	}
+
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
